00b45ddfde4ef182fba7e4df8c70b4093183bae5
SpecialSmiler <358317446@qq.com>
2021-02-06 14:10:42 +0800

    完成窗体大致布局和功能

---
+++ DynamicScreenCapture/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Dynamic_Screen_Capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 8, 3, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "图片保存路径";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 21);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(353, 29);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "截图间隔";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 21);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "毫秒";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.textBox2);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(26, 52);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(152, 37);
+            this.flowLayoutPanel2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 109);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ DynamicScreenCapture/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Dynamic_Screen_Capture
+{
+    public partial class Form1 : Form
+    {
+        bool isCapturing = false;   //是否正在截图
+        string filePath = "D:\\ImageTemp\\";     //默认文件保存路径。
+        int timeInterval = 1000;    //单位ms
+            
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = filePath;
+            textBox2.Text = timeInterval.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //截取屏幕图像
+        public Bitmap GetScreen()
+        {
+            Rectangle ScreenArea = Screen.GetWorkingArea(this);
+            Bitmap bmp = new Bitmap(ScreenArea.Width,ScreenArea.Height);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new Size(ScreenArea.Width, ScreenArea.Height));
+            }
+
+            return bmp;
+        }
+
+        //保存屏幕图像为文件
+        public void SaveImage()
+        {
+            string fileName = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+            string path = filePath + fileName + ".bmp";
+            Bitmap bmp = GetScreen();
+            bmp.Save(path);
+            //bmp.Dispose();
+        }
+
+
+        //开始截图（录制）
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!isCapturing)
+            {
+                isCapturing = true;
+
+                //时间间隔
+                timeInterval = Int32.Parse(textBox2.Text);
+                timer1.Interval = timeInterval;
+                textBox2.Enabled = false;
+
+                //button外观
+                button1.BackColor = ColorTranslator.FromHtml("0xDB0909");
+                button1.ForeColor = ColorTranslator.FromHtml("0xFFFFFF");
+                button1.Text = "正在截图...";               
+                timer1.Enabled = true;
+            }
+            else
+            {
+                isCapturing = false;
+
+                textBox2.Enabled = true;
+
+                //button外观
+                button1.BackColor = ColorTranslator.FromHtml("0xE1E1E1");
+                button1.ForeColor = ColorTranslator.FromHtml("0x000000");
+                button1.Text = "开始";
+                timer1.Enabled = false;
+            }
+            
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+
+        //修改文件路径
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if(folderBrowserDialog.ShowDialog()==DialogResult.OK)
+            {
+                filePath = folderBrowserDialog.SelectedPath + "\\"; //记得在后面加上斜杠，表示这是一个目录
+                textBox1.Text = filePath;
+            }
+
+        }
+    }
+    
+}
